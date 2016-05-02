d410067c03ae2400dee9364992af8d814d4ee149
Hannah Hall <hannahkhall13@gmail.com>
2016-05-02 18:32:36 -0500

    bootstrap and light styling to dog food

--- food.js
+++ food.js
@@ -6,28 +6,20 @@ getDogFood.send();
 
 function printToDom () {
 	var dogFood= JSON.parse(this.responseText);
-	// dogFood= dogFood.dog_brands[0].types;
-	console.log("the Whole thing: ", dogFood.dog_brands);
-
-// Keys for Chuck Wagon
-	console.log("chuck wagon types array: ", dogFood.dog_brands[0].types);//loop.length);
-	console.log("type name: ", dogFood.dog_brands[0].types[0].type);
-	console.log("volumes array", dogFood.dog_brands[0].types[0].volumes.length); //loop.length
-
-// Keys for Purina
-	console.log("purina types: ", dogFood.dog_brands[1].types);
-var string="";
-// For loops to add to DOM
+	var string="";
+	// For loops to add to DOM
 	for (i=0; i<dogFood.dog_brands.length; i++) {
-		string+= "<div class='col-md-3'>"+"<h2>" + dogFood.dog_brands[i].name + "</h2>";
+		string+= `<div class='col-md-6'><h2>${dogFood.dog_brands[i].name}</h2>`;
 		for(j=0; j<dogFood.dog_brands[0].types.length; j++) {
-			string+="<p> Type: " + dogFood.dog_brands[j].types[j].type + "</p>";
+			string+=`<div class='col-md-6 innerRow'><h4>${dogFood.dog_brands[j].types[j].type}</h4>`;
 			for(x=0; x<dogFood.dog_brands[0].types[0].volumes.length; x++) {
-				string+=	"<p> Volume: " + dogFood.dog_brands[x].types[x].volumes[x].name + "</p>" +
-														"<p> Price: " + dogFood.dog_brands[x].types[x].volumes[x].price + "</p>";
+				string+=`<div class='col-md-6 innerRow'><p>Volume: ${dogFood.dog_brands[x].types[x].volumes[x].name}</p>
+								 <p>Price: ${dogFood.dog_brands[x].types[x].volumes[x].price} </p>`;
+				string+=`</div>`;
 			}
+			string+=`</div>`;
 		}
- 		string+= "</div>";
+ 		string+= `</div>`;
  	}
  	inputEl.innerHTML=  string;
 }
